1bfd6df23110d10e346fa70046a77c038cec10d6
itsmeang <32579339+itsmeang@users.noreply.github.com>
2018-12-13 16:57:36 -0500

    Initial commit

---
+++ app/src/main/java/com/getcivix/app/LoginPage.java
@@ -0,0 +1,108 @@
+package com.getcivix.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.seniortasse.civixteam.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginPage extends Activity implements View.OnClickListener{
+
+    // Declare an instance of FirebaseAuth
+    private FirebaseAuth mAuth;
+
+    /* Declare variables for the widgets */
+    EditText editTextEmail, editTextPassword;
+    /* Declare the Welcome Page buttons (button "Log Me In",
+     button "Register New User" */
+    Button buttonLogin,buttonRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        /*Get references to the widgets*/
+        buttonRegister = findViewById(R.id.buttonRegister);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        editTextEmail = findViewById(R.id.editTextEMail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        //Make buttons listening to clicks
+        buttonLogin.setOnClickListener(this);
+        buttonRegister.setOnClickListener(this);
+
+        // Initialize the FirebaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onClick(View v) {
+               /* When user clicks on button "Register New User",
+        then ...*/
+        if(v == buttonRegister) {
+
+            /*An account is created with User Email and Password as entered
+            the edit texts in case the entries are valid.
+            A Success message is returned on successful registration.
+            A Fail message is returned on unsuccessful registration*/
+            mAuth.createUserWithEmailAndPassword(editTextEmail.getText().toString(), editTextPassword.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                //Sign in success, update UI with the signed-in user's information
+                                Toast.makeText(LoginPage.this, "You are successfully registered. Let's go in the store.", Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(LoginPage.this, "The registration failed. Please, review your credentials.", Toast.LENGTH_SHORT).show();
+                            }
+
+                            // ...
+                        }
+                    });
+
+        }
+        /*When user clicks on button "Log Me In",
+        then ...*/
+        else if(v == buttonLogin) {
+
+            /*User is signed in his account if his/her credentials are correctly
+            entered. */
+            mAuth.signInWithEmailAndPassword(editTextEmail.getText().toString(), editTextPassword.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(LoginPage.this, "Login successful", Toast.LENGTH_SHORT).show();
+                                Intent intentHomePage = new Intent(LoginPage.this, HomePage.class);
+                                startActivity(intentHomePage);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(LoginPage.this, "Access denied. You may register first before signing in.", Toast.LENGTH_SHORT).show();
+                            }
+
+                            // ...
+                        }
+                    });
+
+
+        }
+
+        //mAuth.signOut();
+    }
+}
---
+++ app/src/main/java/com/getcivix/app/StatusPage.java
@@ -0,0 +1,70 @@
+package com.getcivix.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.seniortasse.civixteam.R;
+
+public class StatusPage extends Activity  implements View.OnClickListener{
+
+    // Bottom navigation buttons
+    Button buttonHome;
+    Button buttonMap;
+    Button buttonList;
+    Button buttonStatus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status_page);
+
+        buttonHome = findViewById(R.id.buttonHome);
+        buttonMap = findViewById(R.id.buttonMap);
+        buttonList = findViewById(R.id.buttonList);
+        buttonStatus = findViewById(R.id.buttonStatus);
+
+
+        buttonHome.setOnClickListener(this);
+        buttonMap.setOnClickListener(this);
+        buttonList.setOnClickListener(this);
+        buttonStatus.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        // If we click on "Home" button, then...
+        // we should land on Home Page
+        if (v == buttonHome) {
+            Intent intentHome = new Intent(this, HomePage.class);
+            startActivity(intentHome);
+        }
+
+        // If we click on "Map" button, then...
+        // we should land on Map Page
+
+        else if (v == buttonMap) {
+            Intent intentMap = new Intent(this, MapPage.class);
+            startActivity(intentMap);
+        }
+
+        // If we click on "List" button, then...
+        // we should land on List Page
+        else if (v == buttonList) {
+            Intent intentList = new Intent(this, ListPage.class);
+            startActivity(intentList);
+        }
+        // If we click on "Status" button, then...
+        // we should land on Status Page
+        /*
+        else if (v == buttonStatus) {
+            Intent intentStatus = new Intent(this, StatusPage.class);
+            startActivity(intentStatus);
+        }
+        */
+
+    }
+}
