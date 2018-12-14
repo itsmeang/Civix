58e0ab815581d13d2a4fa9abb05ddbdf4405f58a
omerotem2 <omerotem@umich.edu>
2018-12-13 21:42:39 -0500

    with map

--- app/src/main/java/com/getcivix/app/MapPage.java
+++ app/src/main/java/com/getcivix/app/MapPage.java
@@ -1,69 +1,164 @@
 package com.getcivix.app;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.seniortasse.civixteam.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-public class MapPage extends Activity implements View.OnClickListener{
-
-    // Bottom navigation buttons
-    Button buttonHome;
-    Button buttonMap;
-    Button buttonList;
-    Button buttonStatus;
+public class MapPage extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: map is ready");
+        mMap = googleMap;
+
+        if (mLocationPermissionGranted) {
+            getDeviceLocation();
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    private static final String TAG="MapActivity";
+
+    private static final String FINE_LOCATION= Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION= Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE=1234;
+    private static final float DEFAULT_ZOOM=15;
+
+
+    //vars
+    private Boolean mLocationPermissionGranted=false;
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_page);
 
-        buttonHome = findViewById(R.id.buttonHome);
-        buttonMap = findViewById(R.id.buttonMap);
-        buttonList = findViewById(R.id.buttonList);
-        buttonStatus = findViewById(R.id.buttonStatus);
+        getLocationPermission();
+    }
+    private void getDeviceLocation(){
+        Log.d(TAG, "getDeviceLocation: getting the device's current location");
+
+        mFusedLocationProviderClient=LocationServices.getFusedLocationProviderClient(this);
+
+        try{
+            if (mLocationPermissionGranted) {
+                Task location=mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()) {
+                            Log.d(TAG, "onComplete: found location");
+                            Location currentLocation=(Location)task.getResult();
+
+                            moveCamera(new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude()),
+                                    DEFAULT_ZOOM);
+
+                        }else{
+                            Log.d(TAG, "onComplete: current location is null");
+                            Toast.makeText(MapPage.this, "unable to get current location",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }catch(SecurityException e){
+
+            Log.e(TAG, "getDeviceLocation: SecurityException"+ e.getMessage() );
+        }
+    }
+
+    private void moveCamera(LatLng latlng, float zoom){
+        Log.d(TAG, "moveCamera: moving the camera to: lat: "+latlng.latitude+ ", lng: "+latlng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng,zoom));
+
+    }
+
+    private void initMap(){
+        Log.d(TAG, "initMap: initializing map");
+        SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(MapPage.this);
+    }
 
 
-        buttonHome.setOnClickListener(this);
-        buttonMap.setOnClickListener(this);
-        buttonList.setOnClickListener(this);
-        buttonStatus.setOnClickListener(this);
+    private void getLocationPermission(){
+        Log.d(TAG, "getLocationPermission: getting location permissions");
+        String[]permissions={Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionGranted=true;
+                initMap();
+            }else {
+                ActivityCompat.requestPermissions(this,
+                        permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }else{
+            ActivityCompat.requestPermissions(this,
+                    permissions,
+                    LOCATION_PERMISSION_REQUEST_CODE);
+
+        }
     }
 
     @Override
-    public void onClick(View v) {
-        // If we click on "Home" button, then...
-        // we should land on Home Page
-        if (v == buttonHome) {
-            Intent intentHome = new Intent(this, HomePage.class);
-            startActivity(intentHome);
-        }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: called");
+        mLocationPermissionGranted=false;
 
-        // If we click on "Map" button, then...
-        // we should land on Map Page
-        /*
-        else if (v == buttonMap) {
-            Intent intentMap = new Intent(this, MapPage.class);
-            startActivity(intentMap);
-        }
-        */
+        switch(requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:{
+                if(grantResults.length>0){
+                    for(int i=0;i<grantResults.length;i++){
+                        if(grantResults[i]!=PackageManager.PERMISSION_GRANTED)
+                            mLocationPermissionGranted=false;
+                        Log.d(TAG, "onRequestPermissionsResult: permission failed");
+                        return;
 
-        // If we click on "List" button, then...
-        // we should land on List Page
-        else if (v == buttonList) {
-            Intent intentList = new Intent(this, ListPage.class);
-            startActivity(intentList);
+                    }
+                }
+                Log.d(TAG, "onRequestPermissionsResult: permission granted");
+                mLocationPermissionGranted=true;
+                //initialize our map
+                initMap();
+            }
         }
-        // If we click on "Status" button, then...
-        // we should land on Status Page
-        else if (v == buttonStatus) {
-            Intent intentStatus = new Intent(this, StatusPage.class);
-            startActivity(intentStatus);
-        }
-
-
     }
+
+
 }
+
